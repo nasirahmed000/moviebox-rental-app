7bd4a1968ab1c25d0b8c0c875e19f5456c59eb4c
Nasir Ahmed <ahsannasirahmed@gmail.com>
2025-11-24 01:40:32 +0600

    components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+      <section>
+        <p className="text-lg text-gray-700">
+          This is a sample React application using Tailwind CSS for styling.
+        </p>
+      </section>
     </>
   );
 }
